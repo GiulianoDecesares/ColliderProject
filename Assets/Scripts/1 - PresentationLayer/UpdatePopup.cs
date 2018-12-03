d8c843b407e670870bcfa90093683b502ec46303
Giuliano <Giulianodecesares@gmail.com>
2018-12-03 02:20:01 -0300

    Project main structure created. Update system partially created for testing proposes.

---
+++ Assets/Scripts/1 - PresentationLayer/UpdatePopup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatePopup : MonoBehaviour
+{
+    [SerializeField] private Text messageToShow;
+
+    [SerializeField] private Button acceptButton;
+    [SerializeField] private Button cancelButton;
+
+    private void Awake()
+    {
+        
+
+    }
+
+    public void Populate()
+    {
+        this.messageToShow.text = "Looks like your game needs an update!";
+
+        Text acceptButtonText = this.acceptButton.GetComponentInChildren<Text>();
+        Text cancelButtonText = this.cancelButton.GetComponentInChildren<Text>();
+
+        acceptButtonText.text = "Update";
+        cancelButtonText.text = "Exit";
+
+        //this.acceptButton.onClick +=
+    }
+}
