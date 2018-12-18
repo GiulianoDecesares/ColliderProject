e55629f41c71e02dc8fccc8b44da9c96848cf3f7
Giuliano <Giulianodecesares@gmail.com>
2018-12-17 21:15:37 -0300

    Wireframe updated. Character card integrated.

---
+++ Assets/Scripts/1 - PresentationLayer/CharacterCard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterCard : MonoBehaviour
+{
+    private enum State { OCCLUDED, NORMAL, HIGHLIGHTED, DEPLOYED };
+    private State currentState;
+
+    void Start()
+    {
+        this.currentState = State.NORMAL;
+    }
+}
