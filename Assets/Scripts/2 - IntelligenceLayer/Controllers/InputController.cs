39dbaae44dd7a84c6097366bd52684491f06f1fa
Giuliano <Giulianodecesares@gmail.com>
2018-12-18 23:16:05 -0300

    Input manager prototype added

---
+++ Assets/Scripts/2 - IntelligenceLayer/Controllers/InputController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputController : MonoBehaviour, IPointerClickHandler, IDragHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // This method will be executed when clicking over the InputController object in the PlayDialog
+        // Al the touch related information is in the parameter eventData
+
+        Debug.Log("Pointer click detected");
+        Debug.Log("Click position is " + eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // Same as the previous method
+
+        Debug.Log("Pointer dragging detected");
+        Debug.Log("Dragging position is " + eventData.delta);
+    }
+}
